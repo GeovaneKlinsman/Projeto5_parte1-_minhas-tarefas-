af7c23e8bb2ab1dceafa61fd69e95557a5cc1094
GeovaneKlinsman <geovanegondim91@gmail.com>
2025-01-08 19:25:01 -0300

    Criando tarefas (ok)

--- src/components/tarefa/index.tsx
+++ src/components/tarefa/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+
 import * as S from './styles'
 
 import { remover, editar } from '../../store/reducers/tarefas'
 import TarefaClass from '../../models/Tarefa'
+import { BotaoSalvar } from '../../styles'
 
 type Props = TarefaClass
 
@@ -46,7 +48,7 @@ const Tarefa = ({
       <S.barraAcoes>
         {estaEditando ? (
           <>
-            <S.BotaoSalvar
+            <BotaoSalvar
               onClick={() => {
                 dispatch(
                   editar({
@@ -61,7 +63,7 @@ const Tarefa = ({
               }}
             >
               Salvar
-            </S.BotaoSalvar>
+            </BotaoSalvar>
             <S.BotaoCancelarRemover onClick={cancelarEdicao}>
               Cancelar
             </S.BotaoCancelarRemover>
